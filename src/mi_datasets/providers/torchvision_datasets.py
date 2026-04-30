3107d0c625dbaed38e76c2df7d537dae38b3c62f
Erik V. <100310971+VoErik@users.noreply.github.com>
2026-04-30 14:47:05 +0200

    Modify integrity check for CelebA dataset loading
    Commented out the integrity check for the CelebA dataset.

--- src/mi_datasets/providers/torchvision_datasets.py
+++ src/mi_datasets/providers/torchvision_datasets.py
@@ -82,8 +82,10 @@ class CelebADataset(BaseMIDataset):
         pass # Handled by torchvision download=True
 
     def _load_into_memory(self) -> None:
-        
-        setattr(torchvision.datasets.CelebA, "_check_integrity", lambda self: True)
+
+        # might want to uncomment this line if the integrity check fails or you downloaded the 
+        # dataset manually.
+        #setattr(torchvision.datasets.CelebA, "_check_integrity", lambda self: True)
         
         self.tv_dataset = torchvision.datasets.CelebA(
             root=str(Path(self.cache_dir).parent), # torchvision celeba is quirky like that :)
